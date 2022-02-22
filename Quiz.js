b5acd32083d1bbaf94fb105aa9f3ffe847e5f054
noorimanisha <98839524+noorimanisha@users.noreply.github.com>
2022-02-22 15:55:05 +0530

    quizd24

--- Quiz.js
+++ Quiz.js
@@ -14,4 +14,26 @@ const quizData=[
         c: "polan",
         correct: "c",
     },
+    {
+        question: "why do many bees have to starve",
+        a: "because monocultures flower at the same time and bees do not find enough food before and afterwards ",
+        b: "because many bees are specialised on certain plants",
+        c: "because beesonly move within a small radius of their hive",
+        correct: "a",
+
+    },
+    {
+        question: "who produces of the world's food?",
+        a: "organic farmers",
+        b: "large corporations",
+        c: "small farmers",
+        correct: "c",
+    },
+    {
+        question: "what do our meat consumption have to do with the species rich rainforest?",
+        a: "The cattle eat the bark of the trees so that the trees die off ",
+        b: "for every steak a tree is planted in Brazil",
+        c: "Rainforest is cleared for pasture and to grow feed for the European market ",
+        correct: "c",
+    },
     
