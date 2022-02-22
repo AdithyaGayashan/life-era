f651ae020f7408683152b5a4a916e14a34d77d77
noorimanisha <98839524+noorimanisha@users.noreply.github.com>
2022-02-22 15:51:08 +0530

    quizd23

---
+++ Quiz.js
@@ -0,0 +1,17 @@
+const quizData=[
+    {
+        question: "what is the main cause of global extinction species?",
+        a: "the disteuction of natural habitats",
+        b: "poaching and illegal trade ",
+        c: "climate change",
+        correct: "a",
+
+    },
+    {
+        question: "in which Eropean country are the largest areas of primeval forest perseved?",
+        a: "Bulgaria",
+        b: "Finland",
+        c: "polan",
+        correct: "c",
+    },
+    
